28adf43261d706ace1bf2a1c2e32b6c995dca32b
cmroonn <49814994+cmroonn@users.noreply.github.com>
2020-05-19 15:19:57 +0500

    commit

---
+++ js/ajax.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+
+    $("#sender").click(function(e){
+        e.preventDefault();
+        sendAjaxForm('result_form', 'form', '../mail/sender.php');
+    });
+     
+    function sendAjaxForm(result_form, ajax_form, url) {
+        $.ajax({
+            url:     url, //url страницы 
+            type:     "POST", //метод отправки
+            dataType: "html", //формат данных
+            data: $("#"+ajax_form).serialize(),  // Сеарилизуем объект
+            success: function(response) { //Данные отправлены успешно
+                $('.success-popup').addClass('show');
+                $('.signup-popup').removeClass('show');
+            },
+            error: function(response) { // Данные не отправлены
+                $('#result_form').html('Ошибка. Данные не отправлены.');
+            }
+         });
+    };
+});
